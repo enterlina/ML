bd17000d5062b1f48c8086187036b86bddf7db1a
Elena Polyakova <enterlina@gmail.com>
2019-03-31 10:07:56 +0300

    Update file

--- HW4_1_Random.py
+++ HW4_1_Random.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import sklearn.metrics as skmetrics
 from matplotlib import pyplot
 from sklearn.metrics import roc_auc_score
+from statistics import mode
 from sklearn import metrics
 import pandas as pd
 import numpy as np
@@ -10,6 +11,7 @@ from sklearn.model_selection import train_test_split
 import math
 import statistics
 from statistics import mode
+import random
 
 
 class Tree_Node:
@@ -22,16 +24,17 @@ class Tree_Node:
 
 
 def convert(df):
-    df_1 = df.label
+    # df_1 = df[:,0]
 
-    df_label = df_1.values
-    for i in range(len(df_label)):
-        if df_label[i] == 'M':
-            df_label[i] = 1
-        elif df_label[i] == 'B':
-            df_label[i] = 0
+    df_label = []
+    # print(df[:, 0])
+    # print(len(df))
+    for i in range(len(df)):
+        if df[i][0] == 'M':
+            df_label.append(1)
+        elif df[i][0] == 'B':
+            df_label.append(0)
 
-    df_label = list(df_label)
     return df_label
 
 
@@ -46,17 +49,17 @@ def counter(label):
         for i in range(len(label)):
             if label[i] == 1:
                 counter += 1
-        # print(len(label), counter / len(label))
         return counter / len(label)
     # else:
     #     return 0
 
 
-def auc_classification(df, label):
+def auc_classification(df, df_label, length):
     auc_dict = dict()
-    for i in range(1, df.shape[1]):
-        value = roc_auc_score(label, df[df.columns[i]])
+    for i in range(0, length):
+        value = roc_auc_score(df_label, df[:, i])
         auc_dict[value] = i
+
     return auc_dict
 
 
@@ -73,11 +76,11 @@ def get_ig(df, df_label, total, criteria):
 
 # calculate all tresholds for all features
 def get_classific(df, df_label):
-    # num_of_features = df.shape[1]
-    features_auc = auc_classification(df, df_label)
+    length = len(df[1, :])
+    features_auc = auc_classification(df, df_label, length)
     classific = []
     for auc, feature in features_auc.items():
-        fpr, tpr, thresholds = metrics.roc_curve(df_label, df[df.columns[feature]])
+        fpr, tpr, thresholds = metrics.roc_curve(df_label, df[:, feature])
         optimal = np.argmax(tpr - fpr)
         optimal_threshold = thresholds[optimal]
         classific.append((feature, optimal_threshold))
@@ -85,7 +88,7 @@ def get_classific(df, df_label):
 
 
 def calc_accuracy(tree_cart, df_test, label_test):
-    for i in range(df_test.shape[0]):
+    for i in range(len(df)):
         predicted_label = get_label_from_tree(tree_cart, df_test[i])
         expected_label = label_test[i]
         if expected_label == 1:
@@ -103,68 +106,60 @@ def calc_accuracy(tree_cart, df_test, label_test):
 
 def split_classific(df, df_label, feature, threshold):
     df_left, label_left, df_right, label_right = [], [], [], []
-    for i in range(0, df.shape[0]):
-        feature_value = df[df.columns[feature]]
-
-        if feature_value[i] >= threshold:
-            df_right.append(feature_value[i])
-            label_right.append(df_label[i])
+    for label_ind, feature_ar in enumerate(df):
+        feature_value = feature_ar[feature]
+        label = df_label[label_ind]
+        if feature_value >= threshold:
+            df_right.append(feature_ar)
+            label_right.append(label)
         else:
-            df_left.append(feature_value[i])
-            label_left.append(df_label[i])
+            df_left.append(feature_ar)
+            label_left.append(label)
     return df_left, label_left, df_right, label_right
 
 
-def class_percent(df_label):
-    num1 = 0
-    num2 = 0
-    for i in range(len(df_label)):
-        if df_label[i] == 0:
-            num1 += 1
-        elif df_label[i] == 2:
-            num2 += 1
-    num1_per = num1 / len(df_label)
-    num2_per = num2 / len(df_label)
-    return num1_per, num2_per
+def calc_p(column, label, criteria):
+    count_a1 = 0
+    count_a2 = 0
+    count_b1 = 0
+    count_b2 = 0
+    ps_1 = 0
+    ps_2 = 0
+    ps_total = 0
+    for i in range(len(column)):
+        if column[i] > criteria:
+            if label[i] == 1:
+                count_a1 += 1
+            else:
+                count_a2 += 1
 
-
-def calc_criteria(df_test):
-    df_average = []
-    for i in range(len(df_test) - 1):
-        df_average.append((df_test[i] + df_test[i + 1]) / 2)
-    return df_average
-
-
-def calc_best_ind(df_node, column, label, df_criteria, function):
-    IG = []
-    if function == 'gini':
-        for i in range(len(df_criteria)):
-            p_1, p_2, ps_1, ps_2, ps_total = calc_p(column, label, df_criteria[i])
-            IG_local = df_node - (ps_1 / ps_total) * gini(p_1) - (ps_2 / ps_total) * gini(p_2)
-            IG.append(IG_local)
-    IG_ind = np.array(IG)
-    ind = np.argpartition(IG_ind, -1)[-1:]
-    return float(IG_ind[ind]), int(ind)
-
-
-def prob_label(tree, sample):
-    threshold = tree.threshold
-    if not threshold:
-        return tree.label, tree.prob
-    else:
-        feature = tree.feature
-        feature_val = sample[feature]
-        if feature_val >= threshold:
-            return prob_label(tree.right, sample)
         else:
-            return prob_label(tree.left, sample)
+            if label[i] == 1:
+                count_b1 += 1
+            else:
+                count_b2 += 1
+    if (count_a1 == 0) and (count_a2 == 0):
+        p_1 = 0
+
+    else:
+        p_1 = count_a1 / (count_a1 + count_a2)
+        ps_1 = count_a1 + count_a2
+
+    if (count_b1 == 0) and (count_b2 == 0):
+        p_2 = 0
+    else:
+        p_2 = count_b1 / (count_b1 + count_b2)
+        ps_2 = count_b1 + count_b2
+
+    ps_total = count_a1 + count_a2 + count_b1 + count_b2
+
+    return p_1, p_2, ps_1, ps_2, ps_total
 
 
 def get_best_classific(df, df_label, total, classific, criteria):
     count = 0
     max_ig = 0
-    # total = df.shape[0]
-    best_rule = None
+    best_classific = None
     for feature, optimal_threshold in classific:
         df_left, label_left, df_right, label_right = split_classific(df, df_label, feature, optimal_threshold)
 
@@ -180,39 +175,82 @@ def get_best_classific(df, df_label, total, classific, criteria):
             count += 1
         if ig > max_ig:
             max_ig = ig
-            best_rule = (feature, optimal_threshold)
-        print(best_rule)
-    return best_rule
+            best_classific = (feature, optimal_threshold)
+
+    return best_classific
 
 
-def cart(df, label, length, criteria, depth):
-    rules = get_classific(df, label)
-    print('rules', rules)
-    depth_counter = 0
+def calc_criteria(df_test):
+    df_average = []
+    for i in range(len(df_test) - 1):
+        df_average.append((df_test[i] + df_test[i + 1]) / 2)
+    return df_average
+
+
+def prob_label(tree, sample):
+    threshold = tree.threshold
+    if not threshold:
+        return tree.label, tree.prob
+    else:
+        feature = tree.feature
+        feature_val = sample[feature]
+        if feature_val >= threshold:
+            return prob_label(tree.right, sample)
+        else:
+            return prob_label(tree.left, sample)
+
+
+def cart(df, df_label, length, depth):
+    if counter(df_label) == 1:
+        return Tree_Node(0, 0, 1, 0, 1)
+    if counter(df_label) == 0:
+        return Tree_Node(0, 0, 0, 0, 0)
+
+    classific = get_classific(df, df_label)
+
+    # depth_counter = 0
     max_depth = 3
     if depth < max_depth:
-        feature, threshold = get_best_classific(df, label, total, rules, criteria)
-        df_left, label_left, df_right, label_right = split_tree(df, label, feature_idx, threshold)
-        depth_counter = depth + 1
-        left_node = cart(df_left, label_left, length, criteria, depth_counter, depth)
-        right_node = cart(df_right, label_right, length, criteria, new_depth, depth)
-        return Tree_Node(left_node, right_node, feature, threshold)
+        feature, threshold = get_best_classific(df, df_label, total, classific, criteria)
+        if (feature != None) and (threshold != None):
+            df_left, label_left, df_right, label_right = split_classific(df, df_label, feature, threshold)
+            depth_counter = depth + 1
+            left_node = cart(df_left, label_left, length, depth_counter)
+            right_node = cart(df_right, label_right, length, depth_counter)
+            return Tree_Node(left_node, right_node, 0, threshold, 0)
+        else:
+            m_feature = mode(set(df_label))
+            probabiliy = counter(df_label) / len(df_label)
+            return Tree_Node(0, 0, m_feature, 0, probabiliy)
+    else:
+        m_feature = mode(set(df_label))
+        probabiliy = counter(df_label) / len(df_label)
+        return Tree_Node(0, 0, m_feature, 0, probabiliy)
 
 
-def tree_bootstrap(df, label):
-    index = np.random.choice(df.shape[0], df.shape[0])
-    return df[index], label[index]
+def tree_bootstrap(df, df_label):
+    index = np.random.choice(len(df), len(df))
+    df_new = []
+    df_label_new = []
+    for i in range(len(df)):
+        df_new.append(df[index[i]])
+        df_label_new.append(df_label[index[i]])
+    return np.asarray(df_new), np.asarray(df_label_new)
 
 
-def random_forest(df, label, criteria='gini'):
-    total = df.shape[0]
+def random_forest(df, df_label, depth, tree_amount):
+    total = len(df)
     tree_array = []
-    for i in range(20):
-        df, label = tree_bootstrap(df, label)
-        tree_cart = cart(df, label, total, 'gini', 0)
+
+    for i in range(tree_amount):
+        df_n, label_n = tree_bootstrap(df, df_label)
+        tree_cart = cart(df_n, label_n, total, depth)
+        print(tree_cart)
         tree_array.append(tree_cart)
     return tree_array
 
+
+# cart(df, df_label, length, depth)
 
 def get_classification_id(df, tree_cart):
     if (tree_cart.threshold != None) and (tree_cart.feature != None):
@@ -250,22 +288,34 @@ def print_auc(auc_value):
     print('ROC-AUC: ', auc_value)
 
 
-# df = pd.read_csv('/Users/alena_paliakova/Google Drive/!Bioinf_drive/02_MachinLearn/HW3/spam.csv')
-df = pd.read_csv('/Users/alena_paliakova/Google Drive/!Bioinf_drive/02_MachinLearn/HW3/cancer.csv')
+# df = pd.read_csv('/Users/alena_paliakova/Google Drive/!Bioinf_drive/02_MachinLearn/HW4/spam.csv')
+df = pd.read_csv('/Users/alena_paliakova/Google Drive/!Bioinf_drive/02_MachinLearn/HW4/cancer2.csv')
+
+df = df.values
+
 df_label = convert(df)
-df['label'] = df_label
-
-df_original = df.drop(columns='label')
-
+df[:, 0] = df_label
+length = len(df[1, :])
 df_train, df_test, label_train, label_test = split(df, df_label)
+p_1 = counter(label_train)
+total = len(df[:, 0])
+criteria = 'gini'
+depth = 3
+tree_amount = 20
 
-df_train, df_test, label_train, label_test = split(df, df_label)
+print(get_ig(df, df_label, total, criteria))
 
+classific = get_classific(df, df_label)
+
+
+print(df_train, df_test, label_train, label_test)
 df_train, label_train = np.array(df_train.drop(columns='label')), np.array(df_train.label)
 tree_array = random_forest(df_train, label_train, criteria='gini')
-
+,
 df_test, label_test = np.array(df_test.drop(columns='label')), np.array(list(label_test.label))
 fpr, tpr, auc_value = roc_curve(df_test, label_test, tree_array)
 
 print_auc(auc_value)
 plot_roc_curve(fpr, tpr)
+print(list(df))
+print(np.random.shuffle(list(df)))
